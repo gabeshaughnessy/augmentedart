46ec00703d6c6db4eb17a76ae9b951e8836c6f84
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-10-30 15:59:13 -0700

    added mime types for svg

--- wp-content/themes/twentythirteen-child/functions.php
+++ wp-content/themes/twentythirteen-child/functions.php
@@ -1,10 +1,25 @@
 <?php
 
+//hide admin bar for layar pages
 function hide_admin_bar() {
 	if (is_page_template('templates/layar-page.php')) {
 		return false;
 	}
 }
 add_filter( 'show_admin_bar', 'hide_admin_bar' );
+
+//allow additional MIME types for uploading files to the media library
+ 
+function aa_custom_upload_mimes( $existing_mimes ) {
+	// add webm to the list of mime types
+	$existing_mimes['webm'] = 'video/webm';
+	$existing_mimes['mp4'] = 'video/mp4';
+	$existing_mimes['svg'] = 'image/svg+xml';
+
+ 
+	// return the array back to the function with our added mime type
+	return $existing_mimes;
+}
+add_filter( 'mime_types', 'aa_custom_upload_mimes' );
 
 ?>
