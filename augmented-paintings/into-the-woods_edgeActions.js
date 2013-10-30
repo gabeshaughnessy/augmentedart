0e7bd9c5a4c71a7ac67b109afa30c041a25ae957
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-10-30 16:54:28 -0700

    updated animation

--- augmented-paintings/into-the-woods_edgeActions.js
+++ augmented-paintings/into-the-woods_edgeActions.js
@@ -1,6 +1,16 @@
 
 (function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
-(function(symbolName){})("stage");
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","stop",function(sym,e){});
+//Edge binding end
+Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","update",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Into-the-Woods-woman}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.play(0);});
+//Edge binding end
+})("stage");
 //Edge symbol end:'stage'
 })(jQuery,AdobeEdge,"EDGE-75241278");
