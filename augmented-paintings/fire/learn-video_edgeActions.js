30c7618eff5a5afcb0dab2a64fc2c78f91191615
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-11-08 12:11:51 -0800

    stop and click play

--- augmented-paintings/fire/learn-video_edgeActions.js
+++ augmented-paintings/fire/learn-video_edgeActions.js
@@ -1,7 +1,11 @@
 
 (function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
-(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4750,function(sym,e){sym.play(0);});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4750,function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Comp_12}","click",function(sym,e){sym.play(0);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
 //Edge binding end
 })("stage");
 //Edge symbol end:'stage'
