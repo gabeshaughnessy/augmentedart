ed7119bf93098c656eefb08d72580c5513323c42
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-11-08 11:56:44 -0800

    added fire animation

---
+++ augmented-paintings/fire/learn-video_edgeActions.js
@@ -0,0 +1,8 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4750,function(sym,e){sym.play(0);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"learn-video");
