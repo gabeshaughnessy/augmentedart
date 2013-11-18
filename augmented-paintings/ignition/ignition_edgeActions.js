fb7087f51b29144bd18a5130813d3730747a4392
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-11-17 16:45:07 -0800

    tracer test

--- augmented-paintings/ignition/ignition_edgeActions.js
+++ augmented-paintings/ignition/ignition_edgeActions.js
@@ -1,6 +1,18 @@
 
 (function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
-(function(symbolName){})("stage");
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Glowballs}","click",function(sym,e){sym.play(0);});
+//Edge binding end
+})("stage");
 //Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'glowball'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.play('glowstart');});
+//Edge binding end
+})("glowball");
+//Edge symbol end:'glowball'
 })(jQuery,AdobeEdge,"EDGE-227757856");
