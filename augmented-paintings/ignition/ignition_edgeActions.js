3bcbc654bc58dbd0c1891bc295ca7044d4a21479
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-11-19 17:20:34 -0800

    ignition layers

--- augmented-paintings/ignition/ignition_edgeActions.js
+++ augmented-paintings/ignition/ignition_edgeActions.js
@@ -3,7 +3,7 @@
 //Edge symbol: 'stage'
 (function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_Glowballs}","click",function(sym,e){sym.play(0);});
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){});
 //Edge binding end
 })("stage");
 //Edge symbol end:'stage'
@@ -15,4 +15,56 @@ Symbol.bindElementAction(compId,symbolName,"${_Glowballs}","click",function(sym,
 //Edge binding end
 })("glowball");
 //Edge symbol end:'glowball'
+
+//=========================================================
+
+//Edge symbol: 'Glowball-group'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Glowballs}","click",function(sym,e){sym.play(0);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+})("Glowball-group");
+//Edge symbol end:'Glowball-group'
+
+//=========================================================
+
+//Edge symbol: 'large_circle'
+(function(symbolName){})("large_circle");
+//Edge symbol end:'large_circle'
+
+//=========================================================
+
+//Edge symbol: 'background'
+(function(symbolName){})("background");
+//Edge symbol end:'background'
+
+//=========================================================
+
+//Edge symbol: 'small-circle'
+(function(symbolName){})("small-circle");
+//Edge symbol end:'small-circle'
+
+//=========================================================
+
+//Edge symbol: 'chair'
+(function(symbolName){})("chair");
+//Edge symbol end:'chair'
+
+//=========================================================
+
+//Edge symbol: 'spanking'
+(function(symbolName){})("spanking");
+//Edge symbol end:'spanking'
+
+//=========================================================
+
+//Edge symbol: 'spanked'
+(function(symbolName){})("spanked");
+//Edge symbol end:'spanked'
+
+//=========================================================
+
+//Edge symbol: 'flames'
+(function(symbolName){})("flames");
+//Edge symbol end:'flames'
 })(jQuery,AdobeEdge,"EDGE-227757856");
