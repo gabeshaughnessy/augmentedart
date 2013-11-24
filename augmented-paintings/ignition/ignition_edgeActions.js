dbe43e0239514c05ddbae1221f7c3677e546da69
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-11-24 14:25:33 -0800

    flames

--- augmented-paintings/ignition/ignition_edgeActions.js
+++ augmented-paintings/ignition/ignition_edgeActions.js
@@ -73,11 +73,25 @@ Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",731,function(sym,e
 //Edge symbol: 'spanking'
 (function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){});
 //Edge binding end
-Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",733,function(sym,e){sym.stop(0);});
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1233,function(sym,e){sym.getComposition().getStage().getSymbol("small-circle").play(0);});
 //Edge binding end
 Symbol.bindElementAction(compId,symbolName,"${_spank_sprite_sheet2}","click",function(sym,e){sym.play(0);});
 //Edge binding end
-Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",301,function(sym,e){sym.getComposition().getStage().getSymbol("spanked").play();});
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",267,function(sym,e){sym.getComposition().getStage().getSymbol("spanked").play();sym.getComposition().getStage().getSymbol('flames').getSymbol("flame_bg").stop('faded-out');});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",633,function(sym,e){sym.getComposition().getStage().getSymbol("flames").getSymbol("Flame1").play(0);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",700,function(sym,e){sym.getComposition().getStage().getSymbol("flames").getSymbol("Flame2").play(0);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",766,function(sym,e){sym.getComposition().getStage().getSymbol("flames").getSymbol("Flame3").play(0);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",832,function(sym,e){sym.getComposition().getStage().getSymbol("flames").getSymbol("Flame4").play(0);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",900,function(sym,e){sym.getComposition().getStage().getSymbol("flames").getSymbol("Flame5").play(0);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",967,function(sym,e){});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2500,function(sym,e){sym.getComposition().getStage().getSymbol('flames').getSymbol("flame_bg").stop(0);sym.stop(0);});
 //Edge binding end
 })("spanking");
 //Edge symbol end:'spanking'
@@ -97,9 +111,15 @@ Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",200,function(sym,e
 //=========================================================
 
 //Edge symbol: 'ames'
-(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Flames2}","click",function(sym,e){sym.play();});
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Flame5}","mouseover",function(sym,e){sym.getSymbol("Flame5").play(0);});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_Flame_1}","click",function(sym,e){sym.getSymbol("Flame_1").play(0);});
+Symbol.bindElementAction(compId,symbolName,"${_Flame4}","mouseover",function(sym,e){sym.getSymbol("Flame4").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Flame3}","mouseover",function(sym,e){sym.getSymbol("Flame3").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Flame2}","mouseover",function(sym,e){sym.getSymbol("Flame2").play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Flame1}","mouseover",function(sym,e){sym.getSymbol("Flame1").play(0);sym.getSymbol("flame_bg").stop('faded-out');});
 //Edge binding end
 })("ames");
 //Edge symbol end:'ames'
@@ -129,4 +149,54 @@ Symbol.bindElementAction(compId,symbolName,"${_headdress2}","click",function(sym
 //Edge binding end
 })("Flame_1");
 //Edge symbol end:'Flame_1'
+
+//=========================================================
+
+//Edge symbol: 'Flame1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1300,function(sym,e){sym.stop(0);});
+//Edge binding end
+})("Flame1");
+//Edge symbol end:'Flame1'
+
+//=========================================================
+
+//Edge symbol: 'Flame2'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1300,function(sym,e){sym.stop(0);});
+//Edge binding end
+})("Flame2");
+//Edge symbol end:'Flame2'
+
+//=========================================================
+
+//Edge symbol: 'Flame3'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1300,function(sym,e){sym.stop(0);});
+//Edge binding end
+})("Flame3");
+//Edge symbol end:'Flame3'
+
+//=========================================================
+
+//Edge symbol: 'Flame4'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1300,function(sym,e){sym.stop(0);});
+//Edge binding end
+})("Flame4");
+//Edge symbol end:'Flame4'
+
+//=========================================================
+
+//Edge symbol: 'Flame5'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1300,function(sym,e){sym.stop(0);});
+//Edge binding end
+})("Flame5");
+//Edge symbol end:'Flame5'
+
+//=========================================================
+
+//Edge symbol: 'flame_bg'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Flames2}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",67,function(sym,e){sym.stop();});
+//Edge binding end
+})("flame_bg");
+//Edge symbol end:'flame_bg'
 })(jQuery,AdobeEdge,"EDGE-227757856");
