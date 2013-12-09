2e5a72ee23e4fb32519268da2636c5e8824d4752
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-12-08 17:40:40 -0800

    scaled for layar

---
+++ augmented-paintings/into-the-woods/into-the-woods-scaled_edgeActions.js
@@ -0,0 +1,52 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_petals}","click",function(sym,e){sym.getComposition().getStage().getSymbol('petals').play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_petal-trigger}","click",function(sym,e){sym.getComposition().getStage().getSymbol('petals').play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_crown_flame_1}","click",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_head}","click",function(sym,e){setTimeout(function(){sym.getSymbol('crown-flame-17').play(0);},100);setTimeout(function(){sym.getSymbol('crown-flame-13').play(0);},200);setTimeout(function(){sym.getSymbol('crown-flame-12').play(0);},300);setTimeout(function(){sym.getSymbol('crown-flame-1').play(0);},400);setTimeout(function(){sym.getSymbol('crown_flame_1').play(0);},500);setTimeout(function(){sym.getSymbol('crown-flame-14').play(0);},600);setTimeout(function(){sym.getSymbol('crown-flame-15').play(0);},700);setTimeout(function(){sym.getSymbol('crown-flame-16').play(0);},800);setTimeout(function(){sym.getSymbol('crown-flame-18').play(0);},900);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'background'
+(function(symbolName){})("background");
+//Edge symbol end:'background'
+
+//=========================================================
+
+//Edge symbol: 'torch'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_torch-sprite-short-desat}","click",function(sym,e){sym.play(0);});
+//Edge binding end
+})("torch");
+//Edge symbol end:'torch'
+
+//=========================================================
+
+//Edge symbol: 'petals'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){});
+//Edge binding end
+})("petals");
+//Edge symbol end:'petals'
+
+//=========================================================
+
+//Edge symbol: 'crown'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_crown-flame-sprite-small}","click",function(sym,e){sym.play(0);});
+//Edge binding end
+})("crown-flame-1");
+//Edge symbol end:'crown-flame-1'
+
+//=========================================================
+
+//Edge symbol: 'horned-god'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_horned-god-sprite-sheet4}","click",function(sym,e){sym.play(0);});
+//Edge binding end
+})("horned-god");
+//Edge symbol end:'horned-god'
+})(jQuery,AdobeEdge,"into-the-woods");
