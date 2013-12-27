2c81e1835a05ea97f807a1d81b82209e87b27cbf
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-12-27 14:56:15 -0800

    tree flashes, flower fades

--- augmented-paintings/into-the-woods/into-the-woods-scaled_edgeActions.js
+++ augmented-paintings/into-the-woods/into-the-woods-scaled_edgeActions.js
@@ -2,17 +2,17 @@
 (function($,Edge,compId){function animateSpriteSheet(sym,spriteSheetName,spriteWidth,spriteHeight,spriteFrames){var currentPos=parseInt(sym.$(spriteSheetName).css('backgroundPositionX'),10);var newPos=currentPos-spriteWidth;if(newPos>=-1*(spriteWidth*spriteFrames)){setTimeout(function(){sym.$(spriteSheetName).css('backgroundPositionX',newPos);},15);}}
 var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
-(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_petals}","click",function(sym,e){sym.getComposition().getStage().getSymbol('petals').play(0);});
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_petals}","click",function(sym,e){sym.getComposition().getStage().getSymbol('petals').play('fade start');});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_petal-trigger}","click",function(sym,e){sym.getComposition().getStage().getSymbol('petals').play(0);});
+Symbol.bindElementAction(compId,symbolName,"${_petal-trigger}","click",function(sym,e){sym.getComposition().getStage().getSymbol('petals').play('fade start');});
 //Edge binding end
 Symbol.bindElementAction(compId,symbolName,"${_crown_flame_1}","click",function(sym,e){});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_head}","click",function(sym,e){setTimeout(function(){sym.getSymbol('crown-flame-17').play(0);},100);setTimeout(function(){sym.getSymbol('crown-flame-13').play(0);},200);setTimeout(function(){sym.getSymbol('crown-flame-12').play(0);},300);setTimeout(function(){sym.getSymbol('crown-flame-1').play(0);},400);setTimeout(function(){sym.getSymbol('crown_flame_1').play(0);},500);setTimeout(function(){sym.getSymbol('crown-flame-14').play(0);},600);setTimeout(function(){sym.getSymbol('crown-flame-15').play(0);},700);setTimeout(function(){sym.getSymbol('crown-flame-16').play(0);},800);setTimeout(function(){sym.getSymbol('crown-flame-18').play(0);},900);});
-//Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_tree}","click",function(sym,e){sym.getSymbol('tree').getSymbol('leftleaf').play(0);sym.getSymbol('tree').getSymbol('leftleaf2').play(0);sym.getSymbol('tree').getSymbol('leftleaf3').play(0);});
+Symbol.bindElementAction(compId,symbolName,"${_tree}","click",function(sym,e){sym.getSymbol('tree').play(0);});
 //Edge binding end
 Symbol.bindElementAction(compId,symbolName,"${_horned-god-symbol}","click",function(sym,e){sym.getSymbol('horned-god-symbol').play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_head2}","click",function(sym,e){setTimeout(function(){sym.getSymbol('crown-flame-17').play(0);},100);setTimeout(function(){sym.getSymbol('crown-flame-13').play(0);},200);setTimeout(function(){sym.getSymbol('crown-flame-12').play(0);},300);setTimeout(function(){sym.getSymbol('crown-flame-1').play(0);},400);setTimeout(function(){sym.getSymbol('crown_flame_1').play(0);},500);setTimeout(function(){sym.getSymbol('crown-flame-14').play(0);},600);setTimeout(function(){sym.getSymbol('crown-flame-15').play(0);},700);setTimeout(function(){sym.getSymbol('crown-flame-16').play(0);},800);setTimeout(function(){sym.getSymbol('crown-flame-18').play(0);},900);});
 //Edge binding end
 })("stage");
 //Edge symbol end:'stage'
@@ -35,6 +35,12 @@ Symbol.bindElementAction(compId,symbolName,"${_horned-god-symbol}","click",funct
 
 //Edge symbol: 'petals'
 (function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4449,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4800,function(sym,e){sym.play(0);});
+//Edge binding end
+Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","play",function(sym,e){});
 //Edge binding end
 })("petals");
 //Edge symbol end:'petals'
@@ -62,19 +68,7 @@ Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){
 //=========================================================
 
 //Edge symbol: 'tree'
-(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_leftleaf}","click",function(sym,e){});
-//Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_leftleaf}","mouseover",function(sym,e){sym.getSymbol('leftleaf').play();});
-//Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_leftleaf}","mouseout",function(sym,e){setTimeout(function(){sym.getSymbol('leftleaf').stop();},2000);});
-//Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_leftleaf2}","mouseover",function(sym,e){sym.getSymbol('leftleaf2').play();});
-//Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_leftleaf2}","mouseout",function(sym,e){setTimeout(function(){sym.getSymbol('leftleaf2').stop();},2000);});
-//Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_leftleaf3}","mouseover",function(sym,e){sym.getSymbol('leftleaf3').play();});
-//Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_leftleaf3}","mouseout",function(sym,e){setTimeout(function(){sym.getSymbol('leftleaf3').stop();},2000);});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.stop(0);});
 //Edge binding end
 })("tree");
 //Edge symbol end:'tree'
