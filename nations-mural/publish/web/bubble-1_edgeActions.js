62a0e4d402a821560a48fec0920b317941378914
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-01-25 15:10:48 -0800

    instagrams

---
+++ nations-mural/publish/web/bubble-1_edgeActions.js
@@ -0,0 +1,14 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'instagramslide'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",36000,function(sym,e){sym.play(0);});
+//Edge binding end
+})("instagramslide");
+//Edge symbol end:'instagramslide'
+})(jQuery,AdobeEdge,"edge-animation");
