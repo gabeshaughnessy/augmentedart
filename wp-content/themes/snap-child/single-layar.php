f448aab31b0e7f77b4b648adf2b4745c0bd37202
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-02-23 19:16:15 -0800

    cleaned things up

--- wp-content/themes/snap-child/single-layar.php
+++ wp-content/themes/snap-child/single-layar.php
@@ -8,7 +8,7 @@ $layar = array(
 	'hotspots' => array(
 		'id' => 1,
 		'anchor' => array(
-			'referenceImage' => 'http://gabesimagination.com/layar/gabes-imagination/sxsw/turtle.html'
+			'referenceImage' => 'turtle'
 			),
 		'object' => array(
 			'size' => 0.5,
@@ -31,39 +31,4 @@ $layar = array(
 	)
 );
 echo json_encode($layar);
-
-/*{
-    "hotspots": [
-    {
-        "object": {
-            "url": "http://gabesimagination.com/layar/gabes-imagination/sxsw/sxsw-design.html",//url pointing to your carousel.html
-            "contentType": "text/html",
-            "viewport": {
-                "height": 233,//define the height of viewport  
-                "width": 600,//define the width of the viewport . width and height define how much of our page will be shown
-                "interactive": true,//users can swipe the carousel
-                "scrollable": false//users cannot scroll  
-            },
-            "size": 0.3//the size of the widget. This is calculated relative to the reference image/Page
-        },
-        "transform": {
-            "translate": {
-                "y": 0,
-                "x": 0,
-                "z": 0.0001
-            },
-            "scale": 1
-        },
-        "anchor": {
-            "referenceImage": "turtle"
-        },
-        "id": 1
-    }    
-    ],
-    "layer": "gabesimagination",
-    "errorString": "Ok",
-    "errorCode": 0,
-    "morePages": false,
-    "refreshInterval": 900
-}*/
 ?>
