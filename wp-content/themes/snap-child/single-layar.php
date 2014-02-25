1d51b5ea87c93a949da9cdad83bdf3c0f09270cb
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-02-24 16:32:05 -0800

    reformatted object

--- wp-content/themes/snap-child/single-layar.php
+++ wp-content/themes/snap-child/single-layar.php
@@ -1,35 +1,37 @@
 <?php
+$hotspots = array();
+$hotspots[0] = array(
+    'object' => array(
+                'url' => 'http://gabesimagination.com/layar/gabes-imagination/sxsw/turtle.html',
+                'contentType' => 'text/html',
+                'viewport' => array(
+                    'height' => 225,
+                    'width' => 600,
+                    'interactive' => true,
+                    'scrollable' => false
+                ),
+                'size' => 0.5
+            ),
+    'transform' => array(
+        'translate' => array(
+            'x' => 0,
+            'y' => 0,
+            'z' => 0,
+        ),
+        'scale' => 1
+    ),  
+    'anchor' => array(
+        'referenceImage' => 'turtle'
+        ),      
+    'id' => 1
+);
 $layar = array(
+    'hotspots' => $hotspots,
 	'layer' => 'gabesimagination',
 	'errorString' => 'Ok',
 	'errorCode' => 0,
-	'morePage' => false,
-	'refreshInterval' => 900,
-	'hotspots' =>
-        array(
-    		'id' => 1,
-    		'anchor' => array(
-    			'referenceImage' => 'turtle'
-    			),
-    		'object' => array(
-    			'size' => 0.5,
-    			'url' => 'http://gabesimagination.com/layar/gabes-imagination/sxsw/turtle.html',
-    			'viewport' => array(
-    				'height' => 225,
-    				'width' => 600,
-    				'interactive' => true,
-    				'scrollable' => false
-    			)
-    		),
-		'transform' => array(
-			'translate' => array(
-                'x' => 0,
-                'y' => 0,
-                'z' => 0,
-			),
-			'scale' => 1
-		)
-	)
+	'morePage' => false
+	
 );
 echo json_encode($layar);
 ?>
