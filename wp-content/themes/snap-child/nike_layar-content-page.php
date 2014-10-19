f0f2fdff323437f7fb183b1a3c6b18f33c3c274f
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-10-19 16:56:40 -0700

    google tag manager

--- wp-content/themes/snap-child/nike_layar-content-page.php
+++ wp-content/themes/snap-child/nike_layar-content-page.php
@@ -57,3 +57,12 @@ var introMessages = new Array();
   
    ?>
 <?php endwhile; ?>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-P2GK66"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P2GK66');</script>
+<!-- End Google Tag Manager -->
