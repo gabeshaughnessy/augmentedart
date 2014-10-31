df56d70b21728b5661cc0a8b582da4ebdbd5091f
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-10-31 16:09:19 -0700

    connect animation

---
+++ nike-vegas/animation-proofs/connect_edgeActions.js
@@ -0,0 +1,58 @@
+
+var nikeFirebase;var dataSnap;if(typeof userID=='undefined'){userID='download_user_id_'+Math.floor((Math.random()*10)+1);}
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){if(jQuery.cookie('FA15-test')=='true'){if(typeof dataSnap=='undefined'){nikeFirebase.child(userID).set('play');}
+else{if(dataSnap.val()!='stop'){nikeFirebase.child(userID).set('stop');sym.play();}
+else if(dataSnap.val()=='stop'){sym.stop();}}}
+else{sym.play('no-access');}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){nikeFirebase=new Firebase("https://nike-fa15-murals.firebaseio.com");nikeFirebase.child(userID).set('play');nikeFirebase.child(userID).on("value",function(snapshot){dataSnap=snapshot;if(snapshot.val()=='play'){if(jQuery.cookie('FA15-test')=='true'){sym.getSymbol('Athlete').play();sym.getSymbol('bg').play('loop');nikeFirebase.child(userID).set('stop');}};if(snapshot.val()=='stop'){if(jQuery.cookie('FA15-test')=='true'){sym.stop();}};if(snapshot.val()=='reset'){if(jQuery.cookie('FA15-test')=='true'){sym.play('reset');}};});if(typeof wrapped!==undefined){sym.$('Button').wrap('<a href="http://www.augmentedart.com/nike-fa15-gtm/page-content/download/" style="width:150px; height:150px; position:relative; top:65px; left:15px; float:left;" ></a>');}
+else{wrapped=true;}});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",39000,function(sym,e){sym.play('loop');});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Athlete_Trigger}","click",function(sym,e){if(jQuery.cookie('FA15-test')=='true'){if(dataSnap.val()=='stop'){nikeFirebase.child(userID).set('play');}
+else{nikeFirebase.child(userID).set('play');}}});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){nikeFirebase.child(userID).set('stop');sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",6043,function(sym,e){nikeFirebase.child(userID).set('stop');sym.play(0);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'no-access-message'
+(function(symbolName){})("no-access-message");
+//Edge symbol end:'no-access-message'
+
+//=========================================================
+
+//Edge symbol: 'background_400400_symbol_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",6043,function(sym,e){sym.play('loop');});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4000,function(sym,e){sym.stop();});
+//Edge binding end
+})("background_400400_symbol_1");
+//Edge symbol end:'background_400400_symbol_1'
+
+//=========================================================
+
+//Edge symbol: 'connect_foreground_400400_symbol_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1944,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2606,function(sym,e){sym.play('loop');});
+//Edge binding end
+})("connect_foreground_400400_symbol_1");
+//Edge symbol end:'connect_foreground_400400_symbol_1'
+
+//=========================================================
+
+//Edge symbol: 'connect_button_120x113px_symbol_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3000,function(sym,e){sym.play(0);});
+//Edge binding end
+})("connect_button_120x113px_symbol_1");
+//Edge symbol end:'connect_button_120x113px_symbol_1'
+})(jQuery,AdobeEdge,"EDGE-116472068");
