ade31cd0394e18cb549c3c721bde37fca1af862d
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-11-03 17:23:05 -0800

    nike sales templates

---
+++ AE-scripts/layar-response.php
@@ -0,0 +1,143 @@
+//api endpoint
+http://www.augmentedart.com/layar/nike-fa15-murals/
+
+//request bin
+http://requestb.in/13r5ff21
+
+//HEADERS
+Connection: close
+User-Agent: Layar/8.3.4 Android/4.4.2 (samsung SM-G900T)
+Via: 1.1 vegur
+Host: requestb.in
+Accept-Encoding: identity
+X-Request-Id: 0deb6e2c-64a0-42c7-b9b9-ba4ab6762c9c
+Total-Route-Time: 0
+Connect-Time: 2
+
+//QUERYSTRING
+lon: -122.7424324
+userId: 2ac1639394c5e9a07380327b5a0e37723de4e436
+lang: EN
+countryCode: US
+layerName: nikefa15939t
+version: 8.3
+accuracy: 100
+action: refresh
+lat: 45.5839729
+recognizedReferenceImage: mural_1
+
+
+s: Array
+(
+    [lang] => EN
+    [countryCode] => US
+    [userId] => 2ac1639394c5e9a07380327b5a0e37723de4e436
+    [lon] => -122.7424324
+    [action] => refresh
+    [version] => 8.3
+    [lat] => 45.5839729
+    [recognizedReferenceImage] => mural_1
+    [layerName] => nikefa15939t
+    [accuracy] => 100
+)
+
+$_GET: Array
+(
+    [lang] => EN
+    [countryCode] => US
+    [userId] => 2ac1639394c5e9a07380327b5a0e37723de4e436
+    [lon] => -122.7424324
+    [action] => refresh
+    [version] => 8.3
+    [lat] => 45.5839729
+    [recognizedReferenceImage] => mural_1
+    [layerName] => nikefa15939t
+    [accuracy] => 100
+)
+
+$_POST: Array
+(
+    [lang] => EN
+    [countryCode] => US
+    [userId] => 2ac1639394c5e9a07380327b5a0e37723de4e436
+    [lon] => -122.7424324
+    [action] => refresh
+    [version] => 8.3
+    [lat] => 45.5839729
+    [recognizedReferenceImage] => mural_1
+    [layerName] => nikefa15939t
+    [accuracy] => 100
+)
+
+$_SERVER: Array
+(
+    [SERVER_SOFTWARE] => Apache
+    [REQUEST_URI] => /layar/nike-fa15-murals/?lang=EN&countryCode=US&userId=2ac1639394c5e9a07380327b5a0e37723de4e436&lon=-122.7424324&action=refresh&version=8.3&lat=45.5839729&recognizedReferenceImage=mural_1&layerName=nikefa15939t&accuracy=100
+    [DOCUMENT_ROOT] => /home2/gabesima/public_html/augmentedart
+    [GATEWAY_INTERFACE] => CGI/1.1
+    [HTTP_ACCEPT_ENCODING] => identity
+    [HTTP_CONNECTION] => close
+    [HTTP_HOST] => www.augmentedart.com
+    [HTTP_USER_AGENT] => Layar/8.3.4 Android/4.4.2 (samsung SM-G900T)
+    [PATH] => /bin:/usr/bin
+    [QUERY_STRING] => lang=EN&countryCode=US&userId=2ac1639394c5e9a07380327b5a0e37723de4e436&lon=-122.7424324&action=refresh&version=8.3&lat=45.5839729&recognizedReferenceImage=mural_1&layerName=nikefa15939t&accuracy=100
+    [REDIRECT_QUERY_STRING] => lang=EN&countryCode=US&userId=2ac1639394c5e9a07380327b5a0e37723de4e436&lon=-122.7424324&action=refresh&version=8.3&lat=45.5839729&recognizedReferenceImage=mural_1&layerName=nikefa15939t&accuracy=100
+    [REDIRECT_STATUS] => 200
+    [REDIRECT_UNIQUE_ID] => VERNWkKT9MkAADo2GnwAAAKF
+    [REDIRECT_URL] => /layar/nike-fa15-murals/
+    [REMOTE_ADDR] => 54.195.49.48
+    [REMOTE_PORT] => 40788
+    [REQUEST_METHOD] => GET
+    [SCRIPT_FILENAME] => /home2/gabesima/public_html/augmentedart/index.php
+    [SCRIPT_NAME] => /index.php
+    [SERVER_ADDR] => 66.147.249.170
+    [SERVER_ADMIN] => webmaster@augmentedart.gabesimagination.com
+    [SERVER_NAME] => www.augmentedart.com
+    [SERVER_PORT] => 80
+    [SERVER_PROTOCOL] => HTTP/1.1
+    [SERVER_SIGNATURE] => <address>Apache Server at www.augmentedart.com Port 80</address>
+
+    [UNIQUE_ID] => VERNWkKT9MkAADo2GnwAAAKF
+    [PHP_SELF] => /index.php
+    [REQUEST_TIME_FLOAT] => 1413762395.0345
+    [REQUEST_TIME] => 1413762395
+)
+
+//This is what a request to a layar content (from a POI request) contains:
+$_SERVER: Array
+(
+    [SERVER_SOFTWARE] => Apache
+    [REQUEST_URI] => /nike-fa15-gtm/mural-1/
+    [DOCUMENT_ROOT] => /home2/gabesima/public_html/augmentedart
+    [GATEWAY_INTERFACE] => CGI/1.1
+    [HTTP_ACCEPT_ENCODING] => gzip
+    [HTTP_ACCEPT_LANGUAGE] => en
+    [HTTP_CONNECTION] => Keep-Alive
+    [HTTP_COOKIE] => _ga=GA1.2.2132909977.1413760905
+    [HTTP_HOST] => www.augmentedart.com
+    [HTTP_USER_AGENT] => Dalvik/1.6.0 (Linux; U; Android 4.4.2; SM-G900T Build/KOT49H)
+    [HTTP_X_LAYAR_OS] => Android
+    [HTTP_X_LAYAR_OS_VERSION] => 4.4.2
+    [HTTP_X_LAYAR_VERSION] => 8.3.4
+    [PATH] => /bin:/usr/bin
+    [QUERY_STRING] => 
+    [REDIRECT_STATUS] => 200
+    [REDIRECT_UNIQUE_ID] => VER2jkKT9MkAABchsB8AAABW
+    [REDIRECT_URL] => /nike-fa15-gtm/mural-1/
+    [REMOTE_ADDR] => 172.56.38.135
+    [REMOTE_PORT] => 35565
+    [REQUEST_METHOD] => GET
+    [SCRIPT_FILENAME] => /home2/gabesima/public_html/augmentedart/index.php
+    [SCRIPT_NAME] => /index.php
+    [SERVER_ADDR] => 66.147.249.170
+    [SERVER_ADMIN] => webmaster@augmentedart.gabesimagination.com
+    [SERVER_NAME] => www.augmentedart.com
+    [SERVER_PORT] => 80
+    [SERVER_PROTOCOL] => HTTP/1.1
+    [SERVER_SIGNATURE] => <address>Apache Server at www.augmentedart.com Port 80</address>
+
+    [UNIQUE_ID] => VER2jkKT9MkAABchsB8AAABW
+    [PHP_SELF] => /index.php
+    [REQUEST_TIME_FLOAT] => 1413772942.97
+    [REQUEST_TIME] => 1413772942
+)
--- nike-vegas/animation-proofs/measure_edgeActions.js
+++ nike-vegas/animation-proofs/measure_edgeActions.js
@@ -5,7 +5,7 @@ var nikeFirebase;if(typeof userID=='undefined'){userID='local_user_id_'+Math.flo
 (function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){if(jQuery.cookie('FA15-test')=='true'){sym.play();}
 else{sym.play('no-access');}});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){nikeFirebase=new Firebase("https://nike-fa15-murals.firebaseio.com");nikeFirebase.child(userID).set('play');nikeFirebase.child(userID).on("value",function(snapshot){if(snapshot.val()=='play'){if(jQuery.cookie('FA15-test')=='true'){sym.getSymbol('Button').play('loop');sym.getSymbol('Athlete').play();sym.getSymbol('BG').play();}};});if(typeof wrapped!==undefined){sym.$('Button').wrap('<a href="http://www.augmentedart.com/nike-fa15-gtm/page-content/download/" style="width:150px; height:150px; top:155px; left:9px; float:left; position:absolute;" ></a>');}
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){nikeFirebase=new Firebase("https://nike-fa15-murals.firebaseio.com");nikeFirebase.child(userID).set('play');nikeFirebase.child(userID).on("value",function(snapshot){if(snapshot.val()=='play'){if(jQuery.cookie('FA15-test')=='true'){sym.getSymbol('Button').play('loop');sym.getSymbol('Athlete').play();sym.getSymbol('BG').play();}};});if(typeof wrapped!==undefined){sym.$('Button').wrap('<a href="http://www.augmentedart.com/nike-fa15-gtm/page-content/measure/" style="width:150px; height:150px; top:155px; left:9px; float:left; position:absolute;" ></a>');}
 else{wrapped=true;}});
 //Edge binding end
 Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",39000,function(sym,e){sym.play('loop');});
@@ -44,9 +44,9 @@ Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){
 //=========================================================
 
 //Edge symbol: 'background_400400_symbol_1'
-(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",555,function(sym,e){sym.stop();nikeFirebase.child(userID).set("stop");});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",555,function(sym,e){nikeFirebase.child(userID).set("stop");sym.stop();});
 //Edge binding end
-Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",781,function(sym,e){sym.stop(0);nikeFirebase.child(userID).set("stop");});
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",781,function(sym,e){sym.play(0);});
 //Edge binding end
 })("background_400400_symbol_1");
 //Edge symbol end:'background_400400_symbol_1'
@@ -54,9 +54,9 @@ Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",781,function(sym,e
 //=========================================================
 
 //Edge symbol: 'foreground_400400_symbol_1'
-(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",947,function(sym,e){sym.stop();nikeFirebase.child(userID).set("stop");});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",947,function(sym,e){nikeFirebase.child(userID).set("stop");sym.stop();});
 //Edge binding end
-Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1387,function(sym,e){sym.stop(0);nikeFirebase.child(userID).set("stop");});
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1387,function(sym,e){sym.play(0);});
 //Edge binding end
 })("foreground_400400_symbol_1");
 //Edge symbol end:'foreground_400400_symbol_1'
@@ -64,9 +64,7 @@ Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1387,function(sym,
 //=========================================================
 
 //Edge symbol: 'measure_button_160x182px_symbol_1'
-(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",916,function(sym,e){sym.stop();});
-//Edge binding end
-Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1749,function(sym,e){sym.stop(0);});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1749,function(sym,e){sym.stop(0);});
 //Edge binding end
 })("measure_button_160x182px_symbol_1");
 //Edge symbol end:'measure_button_160x182px_symbol_1'
