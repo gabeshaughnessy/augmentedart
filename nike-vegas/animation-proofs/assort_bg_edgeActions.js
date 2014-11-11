c44f4f81d6626caa8a21dc9780e89772cfbb084e
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-11-11 10:42:06 -0800

    font size

--- nike-vegas/animation-proofs/assort_bg_edgeActions.js
+++ nike-vegas/animation-proofs/assort_bg_edgeActions.js
@@ -1,5 +1,6 @@
 
 var nikeFirebase;var dataSnap;if(typeof userID=='undefined'){userID='assort_user_id_'+Math.floor((Math.random()*10)+1);}
+else{userID='assort_user_id_'+userID;}
 (function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
 (function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){if(jQuery.cookie('FA15-test')=='true'){if(typeof dataSnap=='undefined'){nikeFirebase.child(userID).set('play');}
@@ -7,17 +8,13 @@ else{if(dataSnap.val()!='stop'){nikeFirebase.child(userID).set('stop');sym.play(
 else if(dataSnap.val()=='stop'){sym.stop();}}}
 else{sym.play('no-access');}});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){nikeFirebase=new Firebase("https://nike-fa15-murals.firebaseio.com");nikeFirebase.child(userID).set('play');nikeFirebase.child(userID).on("value",function(snapshot){dataSnap=snapshot;if(snapshot.val()=='play'){if(jQuery.cookie('FA15-test')=='true'){sym.getSymbol('BG').play();nikeFirebase.child(userID).set('stop');}};if(snapshot.val()=='stop'){if(jQuery.cookie('FA15-test')=='true'){sym.stop();}};if(snapshot.val()=='reset'){if(jQuery.cookie('FA15-test')=='true'){sym.play('reset');}};});if(typeof wrapped!==undefined){sym.$('Button').wrap('<a href="http://www.augmentedart.com/nike-fa15-gtm/page-content/assort/" style="left:5px; top:10px; width:150px; height:150px; position:relative; float:left;" ></a>');}
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){nikeFirebase=new Firebase("https://nike-fa15-murals.firebaseio.com");nikeFirebase.child(userID).set('play');nikeFirebase.child(userID).on("value",function(snapshot){dataSnap=snapshot;if(snapshot.val()=='play'){if(jQuery.cookie('FA15-test')=='true'){sym.getSymbol('BG').play();}};if(snapshot.val()=='stop'){if(jQuery.cookie('FA15-test')=='true'){sym.stop();}};if(snapshot.val()=='reset'){if(jQuery.cookie('FA15-test')=='true'){sym.getSymbol('BG').play('reset');}};});if(typeof wrapped!==undefined){sym.$('Button').wrap('<a href="http://www.augmentedart.com/nike-fa15-gtm/page-content/assort/" style="left:5px; top:10px; width:150px; height:150px; position:relative; float:left;" ></a>');}
 else{wrapped=true;}});
 //Edge binding end
 Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",39000,function(sym,e){sym.play('loop');});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"${_Athlete_Trigger}","click",function(sym,e){if(jQuery.cookie('FA15-test')=='true'){if(dataSnap.val()=='play'){nikeFirebase.child(userID).set('stop');}
+Symbol.bindElementAction(compId,symbolName,"${_Athlete_Trigger}","click",function(sym,e){if(jQuery.cookie('FA15-test')=='true'){if(dataSnap.val()=='stop'){nikeFirebase.child(userID).set('reset');}
 else{nikeFirebase.child(userID).set('play');}}});
-//Edge binding end
-Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",697,function(sym,e){sym.stop();nikeFirebase.child(userID).set('stop');});
-//Edge binding end
-Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1357,function(sym,e){sym.stop(0);nikeFirebase.child(userID).set('stop');});
 //Edge binding end
 })("stage");
 //Edge symbol end:'stage'
@@ -31,16 +28,10 @@ Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1357,function(sym,
 //=========================================================
 
 //Edge symbol: 'assort_background_400400_symbol_1'
-(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",700,function(sym,e){sym.stop();});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1600,function(sym,e){sym.stop();});
 //Edge binding end
-Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1500,function(sym,e){sym.play(0);});
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3417,function(sym,e){sym.stop(0);});
 //Edge binding end
 })("assort_background_400400_symbol_1");
 //Edge symbol end:'assort_background_400400_symbol_1'
-
-//=========================================================
-
-//Edge symbol: 'background-sprite_400400_symbol_1'
-(function(symbolName){})("background-sprite_400400_symbol_1");
-//Edge symbol end:'background-sprite_400400_symbol_1'
 })(jQuery,AdobeEdge,"EDGE-116472068");
