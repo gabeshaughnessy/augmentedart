d8a96ea6cdafaeb3e2fd9056cd1214ab49a1df15
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-12-31 17:48:43 -0800

    player id

--- dungeon-hacker/edge-animations/js/app.js
+++ dungeon-hacker/edge-animations/js/app.js
@@ -5,28 +5,25 @@ var firebaseRef = new Firebase('https://dungeon-hacker.firebaseio.com/');
 
 function Player(playerID){ //pass unique player ID to the constructor.
 	
-	this.playerID = 'player-'+playerID;
+	this.player = 'player-' + playerID;
 	this.playerClass = 'default-class';
 	this.title="Default Player Title";
 	this.description = "The default player description.";
+	alert(this.player);
 	
 	this.addPlayer = function(){ //create a new player entry in the db or replace existing player entry
-	var player = this.playerID;
-		firebaseRef.child('players').set({
-
-			player : {
+		firebaseRef.child('players').child(this.player).set({
 
 				'title' : this.title,
 				'class' : this.playerClass,
 				'description' : this.description,
 
-			}
 
 		});
 	}
 	this.update = function(){
 		//update existing player entry with new data.
-		firebaseRef.child('players').child(this.playerID).update({
+		firebaseRef.child('players').child(this.player).update({
 
 				'title' : this.title,
 				'class' : this.playerClass,
