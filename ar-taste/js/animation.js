2d3aa5cfb58af8643e0e948f78b2dc16aa79b80c
gabeshaughnessy <gabeshaughnessy@gmail.com>
2016-04-07 22:08:06 -0700

    sigil2

--- ar-taste/js/animation.js
+++ ar-taste/js/animation.js
@@ -1,3 +1,23 @@
+Requests = {
+	QueryString : function(item){
+	var svalue = location.search.match(new RegExp("[\?\&]" + item + "=([^\&]*)(\&?)","i"));
+	return svalue ? svalue[1] : svalue;
+	}
+}
+var sigil;
+//Handle the url parameters
+if(typeof Requests.QueryString("Sigil1") !== 'undefined' && Requests.QueryString("Sigil1") == "true"){
+	sigil = "Sigil1";
+}else if(typeof Requests.QueryString("Sigil2") !== 'undefined' && Requests.QueryString("Sigil2") == "true"){
+	sigil = "Sigil2";
+}else if(typeof Requests.QueryString("Sigil3") !== 'undefined' && Requests.QueryString("Sigil3") == "true"){
+	sigil = "Sigil3";
+}else if(typeof Requests.QueryString("Sigil4") !== 'undefined' && Requests.QueryString("Sigil4") == "true"){
+	sigil = "Sigil4";
+}
+
+console.log(sigil);
+
 //load the sprite sheet
 var spriteSheet = new Image();
 spriteSheet.src = 'animations/spritesheets/spriteSheet.png';
@@ -31,5 +51,5 @@ function stopAnimation() {
     clearInterval(loop);
 }
 $(document).ready(function(){
-	$('.sprite-wrapper').append('<div class="sprite loading"></div>');	
+	$('.sprite-wrapper').append('<a href="/ar-taste/one-pager.html?'+sigil+'=true" class="sprite loading"></a>');	
 });
